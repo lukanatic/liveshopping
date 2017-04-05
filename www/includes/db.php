aa6da2d2e5a93f10a4defe0b1ffacf1b56915880
ibrahim lukan <ibrahim8338@yahoo.com>
2017-04-05 01:45:07 +0100

    Setting of Database for envir

---
+++ www/includes/db.php
@@ -0,0 +1,25 @@
+<?php 
+# test.php sandbox
+define('DBNAME','bookstore');
+define('DBUSER','root');
+define('DBPASS','ibrahim');
+
+
+
+try{
+
+#Prepare a PDO instance
+$conn = new PDO('mysql:host = localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+#set verbose error modes
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+
+}
+catch(PDOException $e){
+
+
+  echo $e->getMessage();
+}
+
+
